254794e4c815b8287c3945be49072fe43007065e
10682823 <burrows.rachel3@gmail.com>
2016-10-14 19:29:38 -0600

    NewStarCatcherUploadForAnthony

---
+++ Boundary.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Boundary : MonoBehaviour {
+
+
+	void OnTriggerEnter ()
+	{
+		RestartLevel ();
+	
+	}
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene (0);
+	}
+}
---
+++ RecycleLevel.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RecycleLevel : MonoBehaviour
+{
+    private Vector3 movePos;
+
+    private List<SendToRecycler> recycleList;
+    void SendThisHandler(SendToRecycler _r)
+    {
+        recycleList.Add(_r);
+        print(recycleList.Count);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        recycleList = new List<SendToRecycler>();
+        SendToRecycler.sendThis += SendThisHandler;
+
+
+    }
+
+    void OnTriggerEnter()
+    {
+        movePos.x = Statics.nextPosition;
+        recycleList[0].transform.position = movePos;
+        Statics.nextPosition += Statics.distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
