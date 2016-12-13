0c793d6105cfd81769b75bd860e363acf1798e56
10682823 <burrows.rachel3@gmail.com>
2016-12-13 12:44:02 -0700

    Script

---
+++ GameOverScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameOverScore : MonoBehaviour {
+
+	public Text egScore;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		egScore.text = ("SCORE: " + PlayerController.count);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
